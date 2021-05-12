73c506d5f64960b09a2d90aa3171113ffdec5d30
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-05-12 00:20:52 -0400

    questions completed with original html

---
+++ Assets/JS/quizref.js
@@ -0,0 +1,56 @@
+const quizQuestions =  [ 
+    {
+        questions: "When was the Master and the Margarita written?",
+        possiblities:  {
+            a: "1966",
+            b: "1937",
+            c: "1914"
+        },
+        correctAnswer: "b"
+        
+    },
+    {
+        questions: "What are the dissapearances of people all over Moscow an allegory of?",
+        possiblities:  {
+            a: "Deaths from war",
+            b: "Victims of plague",
+            c: "Stalin's purge"
+        },
+        correctAnswer: "c"
+        
+    },
+
+    {
+        questions: "What does Bezdomny decide about his life while in the mental institution after his encounter with evil spirits?",
+        possiblities:  {
+            a: "He decides to become a Christian",
+            b: "He decides that he is experiencing a psychotic episode",
+            c: "He decides to never write poetry again"
+        },
+        correctAnswer: "c"
+        
+    },
+
+    {
+        questions: "What does Satan say to the Master when he reveals that he has burned his life's work?",
+        possiblities:  {
+            a: "Manuscripts do not burn",
+            b: "There is no need for it where you are going",
+            c: "You must write it again, but this time complete it"
+        },
+        correctAnswer: "a"
+        
+    },
+
+    {
+        questions: "What allusion does Woland's entourage make about his true nature when asked about the mystical events coming from apartment no. 50",
+        possiblities:  {
+            a: "Knowing won't do you any good",
+            b: "We're not here to make any trouble, only fixing the primus stove",
+            c: "Devil knows what happened"
+        },
+        correctAnswer: "c"
+        
+    },
+
+]
---
+++ pseudocodescript.js
@@ -0,0 +1,25 @@
+// create another javascript file called questions
+// question bank with the answers in an array
+// object which states the correct answer
+example
+// in an array 
+var questions = [ {
+title: "What color is the sky?", 
+choices: ["Purple", "blue", "red"],
+answer:blue }
+
+]
+
+// can use dot notation to loop through questions and answers to print on the screen
+//make choices clickable
+//in main javascript we can say questions.choicesforEach("WRITE FUNCTION") when user clicks on attribute we can check 
+// dot notation is questions.answer will read out answer against user choice. if you use for each, it'll go from questions.answer for each different question
+
+
+var health = [
+    topic:"teeth",
+    products: ["toothbrush", "toothpaste", "mouthwash"]
+    //how do you get to toothpaste in this array?  to print it out? health.products[1]
+
+
+]
