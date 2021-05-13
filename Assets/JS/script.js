e43cecdbff686ef878804376a06d8b1a093f5390
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-05-13 02:07:27 -0400

    functions

---
+++ Assets/JS/script.js
@@ -0,0 +1,23 @@
+//variables
+var quizQuestionContainer = document.getElementById("quiz-question")
+
+
+
+//Initiate quiz and fire on click
+var initButton = document.getElementById("init-btn");
+
+initButton.addEventListener("click", executeQuiz);
+
+
+//INITIALIZE CODE AND BEGIN TO EXECUTE QUIZ
+
+
+function executeQuiz() {
+    //add hide button after we move on
+        function getNextQuestion()
+
+    
+    };
+
+
+function getNextQuestion() {}
