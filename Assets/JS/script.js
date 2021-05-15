00f08623c5dae7d4218dbc54e1d62fa77c877f9f
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-05-15 16:23:23 -0400

    moving questions

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,4 +1,70 @@
 //variables
+//QUESTIONS
+var questions =  [ 
+
+    {
+        userQuestions: "When was the Master and the Margarita written?",
+        userAnswers:  [
+            { choice: "1966", correct: false},
+            { choice: "1937", correct: true},
+            { choice: "1914", correct: false}
+        ],
+        
+        
+    },
+    {
+        userQuestions: "What are the dissapearances of people all over Moscow an allegory of?",
+        userAnswers:  [
+            {choice: "Deaths from war", correct: false },
+            {choice: "Victims of plague", correct: false},
+            {choice: "Stalin's purge", correct: true }
+        ]
+        
+        
+    },
+
+    {
+        userQuestions: "What does Bezdomny decide about his life while in the mental institution after his encounter with evil spirits?",
+        userAnswers:  [
+            {choice: "He decides to become a Christian", correct: false},
+            {choice: "He decides that he is experiencing a psychotic episode", correct: false},
+            {choice: "He decides to never write poetry again", correct: true}
+        ]
+        
+    },
+
+    {
+        userQuestions: "What does Satan say to the Master when he reveals that he has burned his life's work?",
+        userAnswers:  [
+            {choice: "Manuscripts do not burn", correct: true},
+            {choice: "There is no need for it where you are going", correct: false},
+            {choice: "You must write it again, but this time complete it", correct: false}
+        ]
+        
+        
+    },
+
+    {
+        userQuestions: "What allusion does Woland's entourage make about his true nature when asked about the mystical events coming from apartment no. 50",
+        userAnswers:  [
+            {choice: "Knowing won't do you any good", correct: false},
+            {choice: "We're not here to make any trouble, only fixing the primus stove", correct: false},
+            {choice: "Devil knows what happened", correct: true}
+        ],
+        
+    },
+
+];
+
+//END QUESTIONS
+
+
+
+
+
+
+
+
 var questionTextEl = document.getElementbyId=("question-text");
 var initButton = document.getElementById("start");
 var answerSelectorsElement = document.getElementById("answer-selectors");
@@ -72,61 +138,3 @@ function displayCurrentQuestion(question, quizQuestions) {
 
 
 
-//QUESTIONS
-var questions =  [ 
-
-    {
-        userQuestions: "When was the Master and the Margarita written?",
-        userAnswers:  [
-            { choice: "1966", correct: false},
-            { choice: "1937", correct: true},
-            { choice: "1914", correct: false}
-        ],
-        
-        
-    },
-    {
-        userQuestions: "What are the dissapearances of people all over Moscow an allegory of?",
-        userAnswers:  [
-            {choice: "Deaths from war", correct: false },
-            {choice: "Victims of plague", correct: false},
-            {choice: "Stalin's purge", correct: true }
-        ]
-        
-        
-    },
-
-    {
-        userQuestions: "What does Bezdomny decide about his life while in the mental institution after his encounter with evil spirits?",
-        userAnswers:  [
-            {choice: "He decides to become a Christian", correct: false},
-            {choice: "He decides that he is experiencing a psychotic episode", correct: false},
-            {choice: "He decides to never write poetry again", correct: true}
-        ]
-        
-    },
-
-    {
-        userQuestions: "What does Satan say to the Master when he reveals that he has burned his life's work?",
-        userAnswers:  [
-            {choice: "Manuscripts do not burn", correct: true},
-            {choice: "There is no need for it where you are going", correct: false},
-            {choice: "You must write it again, but this time complete it", correct: false}
-        ]
-        
-        
-    },
-
-    {
-        userQuestions: "What allusion does Woland's entourage make about his true nature when asked about the mystical events coming from apartment no. 50",
-        userAnswers:  [
-            {choice: "Knowing won't do you any good", correct: false},
-            {choice: "We're not here to make any trouble, only fixing the primus stove", correct: false},
-            {choice: "Devil knows what happened", correct: true}
-        ],
-        
-    },
-
-];
-
-//END QUESTIONS
